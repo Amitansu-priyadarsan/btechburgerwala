7a26202e16d2696e8b72ae2ddbad3b28f91d9ac0
Amitansu Priyadarsan <101704906+Amitansu-priyadarsan@users.noreply.github.com>
2023-06-20 20:20:40 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,36 @@
 import express from "express";
-
-import passport  from "passport";
-import { myProfile , logout, getAdminUsers, getAdminStats } from "../controllers/user.js";
+import passport from "passport";
+import {
+  getAdminStats,
+  getAdminUsers,
+  logout,
+  myProfile,
+} from "../controllers/user.js";
 import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/googleLogin", passport.authenticate("google",
-{
+router.get(
+  "/googlelogin",
+  passport.authenticate("google", {
     scope: ["profile"],
-}));
+  })
+);
 
 router.get(
-    "/login",
-    passport.authenticate("google", {
-        successRedirect: process.env.FRONTEND_URL,
-    }),
-    
+  "/login",
+  passport.authenticate("google", {
+    successRedirect: process.env.FRONTEND_URL,
+  })
 );
 
 router.get("/me", isAuthenticated, myProfile);
 
-router.get("/me",myProfile);
+router.get("/logout", logout);
 
-router.get("/logout",logout);
+// Admin Routes
+router.get("/admin/users", isAuthenticated, authorizeAdmin, getAdminUsers);
 
-router.get("/admin/users", isAuthenticated, authorizeAdmin, getAdminUsers );
-
-router.get("/admin/stats", isAuthenticated, authorizeAdmin ,getAdminStats)
+router.get("/admin/stats", isAuthenticated, authorizeAdmin, getAdminStats);
 
 export default router;
