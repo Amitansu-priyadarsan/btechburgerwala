8f5dc3347485dbdc47d165c0b6779c4e596b7908
Amitansu Priyadarsan <101704906+Amitansu-priyadarsan@users.noreply.github.com>
2023-06-20 21:18:19 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -18,10 +18,11 @@ router.get(
 );
 
 router.get(
-  "/login",
-  passport.authenticate("google", {
-    successRedirect: process.env.FRONTEND_URL,
-  })
+    "/login",
+    passport.authenticate("google"),
+   (req,res,next) => {
+    res.send("Logged in ");
+   },
 );
 
 router.get("/me", isAuthenticated, myProfile);
